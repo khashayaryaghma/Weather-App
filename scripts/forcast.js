d96e07e021fd86e0eeecbbdfd09a45d2573c8007
khashayaryaghma <92808021+khashayaryaghma@users.noreply.github.com>
2022-03-14 19:43:33 +0330

    Update forcast.js

--- scripts/forcast.js
+++ scripts/forcast.js
@@ -2,6 +2,9 @@ const key = "SF0PHiW8pNJny6Z50EY3gdMfI38PvXil";
 const getCity = async(city)=>{
     const base =
       "http://dataservice.accuweather.com/locations/v1/cities/search";
-    const query = `?apiKey=${key}&q=${city}`  
-    const res = await fetch(base + query)
+    const query = `?apikey=${key}&q=${city}`  
+    const res = await fetch(base + query);
+    const data = await res.json()
+    console.log(data);
 }
+getCity("newyork")
