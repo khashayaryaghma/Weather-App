cf01a060d15e4a129822f6afd390ebef0c2a10f6
khashayaryaghma <92808021+khashayaryaghma@users.noreply.github.com>
2022-03-14 20:18:41 +0330

    Update forcast.js

--- scripts/forcast.js
+++ scripts/forcast.js
@@ -1,15 +1,27 @@
 const key = "SF0PHiW8pNJny6Z50EY3gdMfI38PvXil";
+//get wheather
+const getWeather = async(id)=>{
+    const base = "http://dataservice.accuweather.com/currentconditions/v1/";
+    const query = `${id}?apikey=${key}`
+    const res = await fetch(base + query)
+    const data = await res.json()
+    return data;
+}
+
+//get city
 const getCity = async(city)=>{
     const base =
-      "http://dataservice.accuweather.com/loctions/v1/cities/search";
+      "http://dataservice.accuweather.com/locations/v1/cities/search";
     const query = `?apikey=${key}&q=${city}`  
     const res = await fetch(base + query);
     const data = await res.json()
     return data[0]
 }
-getCity("newyork")
-.then((data)=>{
-    console.log(data);
-}).catch((err)=>{
-    console.error("err");
-})
+
+// getCity("newyork")
+// .then((data)=>{
+//     console.log(data);
+// }).catch(()=>{
+//     console.error("somthing was wrong")
+// })
+getWeather("329260")
