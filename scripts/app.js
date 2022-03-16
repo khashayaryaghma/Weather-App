09ff8d73f3237a35e14b3b3fc6b5e0950e4e10d8
khashayaryaghma <92808021+khashayaryaghma@users.noreply.github.com>
2022-03-16 20:34:03 +0330

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -1,17 +1,24 @@
 const cityForm = document.querySelector("form");
+const card = document.querySelector(".card");
+const details = document.querySelector(".details")
+
+const
 
 const updateCity = async (city) => {
   const cityDets = await getCity(city);
   const weather = await getWeather(cityDets.key);
   return {
-    cityDets: cityDets,
-    weather: weather,
+    cityDets,
+    weather,
   };
 };
 cityForm.addEventListener("submit", (e) => {
+
   e.preventDefault();
+
   const city = cityForm.city.value.trim();
   cityForm.reset();
+
   updateCity(city)
   .then((data)=>{
     console.log(data);
